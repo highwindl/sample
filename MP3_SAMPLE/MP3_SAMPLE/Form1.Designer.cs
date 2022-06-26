0e310c4f9f773ec658e5a1f0f0b75f3dc9982077
highwindl <highwindl52@gmail.com>
2022-06-27 00:11:39 +0900

    add mp3 player sample for c#

---
+++ MP3_SAMPLE/MP3_SAMPLE/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MP3_SAMPLE
+{
+    partial class frm_mp3player
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_addMedia = new System.Windows.Forms.Button();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.lbx_playlist = new System.Windows.Forms.ListBox();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_addMedia
+            // 
+            this.btn_addMedia.Location = new System.Drawing.Point(12, 12);
+            this.btn_addMedia.Name = "btn_addMedia";
+            this.btn_addMedia.Size = new System.Drawing.Size(75, 23);
+            this.btn_addMedia.TabIndex = 0;
+            this.btn_addMedia.Text = "파일추가";
+            this.btn_addMedia.UseVisualStyleBackColor = true;
+            // 
+            // btn_play
+            // 
+            this.btn_play.Location = new System.Drawing.Point(93, 12);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(75, 23);
+            this.btn_play.TabIndex = 1;
+            this.btn_play.Text = "재생";
+            this.btn_play.UseVisualStyleBackColor = true;
+            // 
+            // lbx_playlist
+            // 
+            this.lbx_playlist.FormattingEnabled = true;
+            this.lbx_playlist.HorizontalScrollbar = true;
+            this.lbx_playlist.ItemHeight = 12;
+            this.lbx_playlist.Location = new System.Drawing.Point(12, 41);
+            this.lbx_playlist.Name = "lbx_playlist";
+            this.lbx_playlist.Size = new System.Drawing.Size(588, 304);
+            this.lbx_playlist.TabIndex = 2;
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(174, 12);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 3;
+            this.btn_stop.Text = "정지";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            // 
+            // frm_mp3player
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 361);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.lbx_playlist);
+            this.Controls.Add(this.btn_play);
+            this.Controls.Add(this.btn_addMedia);
+            this.Name = "frm_mp3player";
+            this.Text = "MP3 Player";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_addMedia;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.ListBox lbx_playlist;
+        private System.Windows.Forms.Button btn_stop;
+    }
+}
+
---
+++ MP3_SAMPLE/MP3_SAMPLE/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WMPLib; //미디어 플레이어 라이브러리, 최초 프로젝트시 참조에 추가 (COM의 Windows Media Player)
+
+namespace MP3_SAMPLE
+{
+    public partial class frm_mp3player : Form
+    {
+        private List<string> listFileFullPaths = new List<string>(); //파일절대경로를 저장하기 위한 리스트
+        private WindowsMediaPlayer windowsMediaPlayer = new WindowsMediaPlayer();
+
+        private int playFileNum = 0; //현재 재생중인 미디어번호
+        private bool playStatus = false; //플레이 상태
+        private Timer playTimer = new Timer(); //다음곡 자동재생용 타이머
+
+        public frm_mp3player()
+        {
+            InitializeComponent();
+
+            btn_addMedia.Click += Btn_addMedia_Click; //mp3 파일을 열기위한 버튼이벤트
+            btn_play.Click += Btn_play_Click; //mp3파일을 재생하기위한 버튼이벤트
+            btn_stop.Click += Btn_stop_Click; //mp3파일을 정지하기위한 버튼이벤트
+
+            windowsMediaPlayer.PlayStateChange += WindowsMediaPlayer_PlayStateChange; //mediaplayer의 play state가 변경되면 발생하는 이벤트
+
+            playTimer.Enabled = true;
+            playTimer.Stop();
+            playTimer.Tick += PlayTimer_Tick;
+        }
+
+        private void PlayTimer_Tick(object sender, EventArgs e)
+        {
+            playTimer.Stop();
+            playFileNum++; //미디어번호 1 증가
+
+            if (playFileNum >= lbx_playlist.Items.Count) //리스트에 저장된 갯수를 넘으면 다시 0번부터 재생
+            {
+                playFileNum = 0;
+            }
+
+            lbx_playlist.SetSelected(playFileNum, true);
+            windowsMediaPlayer.URL = lbx_playlist.SelectedItem.ToString();
+            windowsMediaPlayer.controls.play();
+        }
+
+        //mediaplayer의 play state가 변경되면 발생하는 이벤트
+        //NewState가 Stopped일때(노래가 끝날때) 다음곡 자동재생하기위해 추가
+
+        private void WindowsMediaPlayer_PlayStateChange(int NewState)
+        {
+            switch (NewState)
+            {
+                case 0:    // Undefined
+                    break;
+
+                case 1:    // Stopped
+                    if (playStatus == true)
+                    {
+                        if (lbx_playlist.Items.Count > 0)
+                        {
+                            playTimer.Start(); //다음곡 재생
+                        }
+                    }
+                    break;
+
+                case 2:    // Paused
+                    break;
+
+                case 3:    // Playing
+                    break;
+
+                case 4:    // ScanForward
+                    break;
+
+                case 5:    // ScanReverse
+                    break;
+
+                case 6:    // Buffering
+                    break;
+
+                case 7:    // Waiting
+                    break;
+
+                case 8:    // MediaEnded
+                    break;
+
+                case 9:    // Transitioning
+                    break;
+
+                case 10:   // Ready
+                    break;
+
+                case 11:   // Reconnecting
+                    break;
+
+                case 12:   // Last
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        //mp3 파일을 열기위한 버튼이벤트
+        private void Btn_addMedia_Click(object sender, EventArgs e)
+        {
+
+            //파일열기
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Environment.CurrentDirectory + "\\mp3"; //최초에 열리는 폴더경로(실행파일 위치의 mp3폴더)
+                openFileDialog.Filter = "mp3 files (*.mp3)|*.mp3"; //파일 필터 설정
+                openFileDialog.Multiselect = true; //파일 다중선택
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string path in openFileDialog.FileNames)
+                    {
+                        if (listFileFullPaths.FindIndex(data => data.Contains(path)) == -1) //중복된 파일인지 확인
+                        {
+                            lbx_playlist.Items.Add(path); //중복되지 않았으면 리스트에 저장
+                        }
+                    }
+                }
+            }
+
+        }
+
+        //mp3파일을 재생하기위한 버튼이벤트
+        private void Btn_play_Click(object sender, EventArgs e)
+        {
+            playStatus = true;
+
+            if (lbx_playlist.SelectedItem != null)
+            {
+                playFileNum = lbx_playlist.SelectedIndex; //현재 재생할 mp3의 index값
+                windowsMediaPlayer.URL = lbx_playlist.SelectedItem.ToString();
+                windowsMediaPlayer.controls.play();
+            }
+            else
+            {
+                MessageBox.Show("재생할 음악을 선택해 주십시오");
+            }
+        }
+
+        //mp3파일을 정지하기위한 버튼이벤트
+        private void Btn_stop_Click(object sender, EventArgs e)
+        {
+            playStatus = false;
+            //음악이 재생중이면 정지
+            if(windowsMediaPlayer.playState == WMPPlayState.wmppsPlaying)
+            {
+                windowsMediaPlayer.controls.stop();
+            }
+        }
+
+
+    }
+}
